c8e2ca0897c2f346cc4b45ea550bc52adf53cb26
foysalpranto121 <foysalpranto2002@gmail.com>
2025-06-16 00:46:12 +0600

    Add new file

---
+++ basic_javascript.js
@@ -0,0 +1,27 @@
+console.log("hello World");
+console.log("bye world");
+// variables
+var friendName ="rahim"
+console.log(friendName);
+// string data type
+var strType='web development'
+console.log(strType);
+console.log( typeof strType);
+// number type datatypes
+var numType= 12355555
+console.log(typeof numType);
+console.log(numType);
+// Boolean dataType 
+var boolType= true;
+console.log(boolType);
+console.log(typeof boolType);
+// reserved keywords
+// General Keywords: These are the most common keywords used for control flow, variable declaration, and other fundamental operations. Examples include: if, else, for, while, do, break, continue, return, switch, case, default, try, catch, finally, throw, var, let, const, function, class, new, this, typeof, instanceof, in, delete, void, debugger.
+// Boolean Literals: true and false represent boolean values.
+// Null Literal: null represents the intentional absence of a value.
+
+
+
+
+
+
