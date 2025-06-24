33e35b8c85bedc7527236974b41dd72706820a09
foysalpranto121 <foysalpranto2002@gmail.com>
2025-06-25 02:08:35 +0600

    Add new file

--- array.js
+++ array.js
@@ -27,3 +27,16 @@ console.log(fndNmae);
 
 fndNmae.unshift("Mahin");
 console.log(fndNmae);
+
+
+// slice () mehod ---> u can sepecifically show  any value from ur lists any part 
+console.log(fndNmae.slice(1,2));// take array[1] ---> kassem output
+// u can taka example as a cake ( slice ) --- cut 1st to end part ( not 3 part at a time )
+console.log(fndNmae.slice(0,2));
+
+
+
+//// splice --->>>  specific way to cut and replace new thing or value 
+let number=[1,2,3,4,5,6,7,8]
+console.log(number.splice(0,4,));
+console.log(number.slice(0,4,));
